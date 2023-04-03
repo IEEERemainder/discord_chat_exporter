4a8d8324f72b7970d0b6b183fb0a87af0deeb0a4
IEEERemainder <nosare@yandex.ru>
2023-04-04 01:50:24 +0500

    Fix tabs in simple_DCE

--- simple_DCE.py
+++ simple_DCE.py
@@ -28,20 +28,20 @@ processed = []
 after = 0
 
 for i in ids:
-	if i in processed:
-		continue
-	processed.append(i)
-	while True:
-		msg_chunc = json.loads(http_get(f'https://discord.com/api/v8/channels/{i}/messages?limit=100&after={after}', {
-	        'Authorization': tocen, 
-	        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36', 
-	        'Content-Type': 'application/json'
-	    }))[::-1]
-	    after = msg_chunc[-1]['id']
-		cursor.executemany('INSERT OR IGNORE INTO messages VALUES (?,?,?,?,?,?,?,?)', [[m['id'], m['author']['id'], m['channel_id'], m['content'], parse_datetime(m['timestamp']), parse_datetime(m['edited_timestamp']), '\t'.join([a['url'] for a in m['attachments']]), '\t'.join([r['emoji']['name'] + ':' + str(r['count']) for r in (m['reactions'] if 'reactions' in m else [])])] for m in msg_chunc])
-	    cursor.executemany('INSERT OR IGNORE INTO users VALUES (?,?,?,?,?)', [[m['author']['id'], m['author']['username'], m['author']['discriminator'], 1 if 'bot' in m['author'] else 0, m['author']['avatar']] for m in msg_chunc])
-	   	if len(msg_chunk) < 100:
-	   		break
-	print("downloaded", i)
-	db.commit()
+    if i in processed:
+        continue
+    processed.append(i)
+    while True:
+        msg_chunc = json.loads(http_get(f'https://discord.com/api/v8/channels/{i}/messages?limit=100&after={after}', {
+            'Authorization': tocen, 
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36', 
+            'Content-Type': 'application/json'
+        }))[::-1]
+        after = msg_chunc[-1]['id']
+        cursor.executemany('INSERT OR IGNORE INTO messages VALUES (?,?,?,?,?,?,?,?)', [[m['id'], m['author']['id'], m['channel_id'], m['content'], parse_datetime(m['timestamp']), parse_datetime(m['edited_timestamp']), '    '.join([a['url'] for a in m['attachments']]), '    '.join([r['emoji']['name'] + ':' + str(r['count']) for r in (m['reactions'] if 'reactions' in m else [])])] for m in msg_chunc])
+        cursor.executemany('INSERT OR IGNORE INTO users VALUES (?,?,?,?,?)', [[m['author']['id'], m['author']['username'], m['author']['discriminator'], 1 if 'bot' in m['author'] else 0, m['author']['avatar']] for m in msg_chunc])
+           if len(msg_chunk) < 100:
+               break
+    print("downloaded", i)
+    db.commit()
 print("done")
